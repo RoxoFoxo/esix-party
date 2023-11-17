0759e84f481476e742bb657aa5aaf1bfbeeb160f
Gabriel Prado <gab.apfprado@gmail.com>
2023-11-17 07:34:26 -0300

    feat: add more image tampering methods

--- lib/core/games/guess_the_tag/image_setup.ex
+++ lib/core/games/guess_the_tag/image_setup.ex
@@ -1,13 +1,24 @@
 defmodule Core.Games.GuessTheTag.ImageSetup do
-  @tampering_methods ~w[pixelate]a
+  @tampering_methods ~w[pixelate ripple random_crop]a
 
   def edit(image_binary) do
     image_binary
     |> Image.from_binary!()
     |> tamper_img(Enum.random(@tampering_methods))
-    |> Image.write!(:memory, suffix: ".jpg")
+    |> Image.write!(:memory, suffix: ".webp")
     |> Base.encode64()
   end
 
-  defp tamper_img(img, :pixelate), do: Image.pixelate!(img, 0.03)
+  defp tamper_img(img, :pixelate),
+    do: img |> thumbnail! |> Image.pixelate!(0.07)
+
+  defp tamper_img(img, :ripple),
+    do: img |> thumbnail! |> Image.ripple!()
+
+  defp tamper_img(img, :random_crop),
+    do: img |> Image.crop!(random_percent(75), random_percent(75), 0.25, 0.25) |> thumbnail!
+
+  defp thumbnail!(img), do: Image.thumbnail!(img, 1080, resize: :down)
+
+  defp random_percent(max), do: 0..max |> Enum.random() |> then(&(&1 * 0.01)) |> Float.round(2)
 end
