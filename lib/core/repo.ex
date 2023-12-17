6c1978d0757f0bb1a2aaccd2cb4b4fc17d0c5647
Gabriel Prado <gab.apfprado@gmail.com>
2023-12-17 19:14:45 -0300

    feat: create release

--- lib/core/repo.ex
+++ lib/core/repo.ex
@@ -1,5 +1,5 @@
 defmodule Core.Repo do
-  use Ecto.Repo,
-    otp_app: :core,
-    adapter: Ecto.Adapters.Postgres
+  #  use Ecto.Repo,
+  #    otp_app: :core,
+  #    adapter: Ecto.Adapters.Postgres
 end
